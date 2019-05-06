6e7dc536dd692476f9c29fbfeb8b8e5462df2f14
WR-BillRich <wr.william77@gmail.com>
2019-05-06 10:50:35 +0800

    Improve the GUI Renew the logic Looks nicer cannot input null
    Functions:
    1. Save : to save current progress and create new module
    2. Calculate : calculate the final results and reset all value

--- app/src/main/java/com/example/gpacalc/MainActivity.java
+++ app/src/main/java/com/example/gpacalc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gpacalc;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,20 +11,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public TextView result;
+    public TextView module;
+
     public Button buttonCalc;
-    double totalGPA=0;
+    public Button buttonSave;
+
+    double totalMark=0;
     double totalCredit=0;
 
-    public TextView textView;
-    public TextView textView2;
-    public TextView textView3;
-    public TextView textView4;
-
-    //variables
     public EditText examGrade;
     public EditText cwGrade;
     public EditText examPercentage;
     public EditText creditModule;
+
+    int counter =1;
 
     private static final String LOG_TAG = "MainActivity";
     @Override
@@ -30,56 +32,125 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //variables
+        //textViews
         result = (TextView) findViewById(R.id.result);
+        module = (TextView) findViewById(R.id.textViewModule1);
+
+        //buttons
+        buttonSave = (Button) findViewById(R.id.buttonSave);
         buttonCalc = (Button) findViewById(R.id.buttonCalc);
-
         //arrays of editText(s)
-        final int[] creditList = new int[] {R.id.editTextModuleCredit1,R.id.editTextModuleCredit2};
-        final int[] gradeList = new int[] {R.id.editTextGrade1,R.id.editTextGrade2};
-        final int[] cwgradeList = new int[] {R.id.editTextCourseWorkGrade1,R.id.editTextCourseWorkGrade2};
-        final int[] percentageList = new int[] {R.id.editTextPercentage1,R.id.editTextPercentage2};
 
-        buttonCalc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                for (int i=0; i<creditList.length;i++) {
-                    //declaring the variables
-                    examGrade = (EditText) findViewById(gradeList[i]);
-                    double grade = Double.parseDouble(examGrade.getText().toString());
-                    cwGrade = (EditText) findViewById(cwgradeList[i]);
-                    double cwgrade = Double.parseDouble(cwGrade.getText().toString());
-                    examPercentage = (EditText) findViewById(percentageList[i]);
-                    double exampercentage = Double.parseDouble(examPercentage.getText().toString());
-                    creditModule = (EditText) findViewById(creditList[i]);
-                    double credit = Double.parseDouble(creditModule.getText().toString());
+                buttonSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        examGrade = (EditText) findViewById(R.id.editTextGrade1);
+                        String sExamGrade = examGrade.getText().toString();
+                        cwGrade = (EditText) findViewById(R.id.editTextCourseWorkGrade1);
+                        String sCwGrade = cwGrade.getText().toString();
+                        examPercentage = (EditText) findViewById(R.id.editTextPercentage1);
+                        String sExamPercentage = examPercentage.getText().toString();
+                        creditModule = (EditText) findViewById(R.id.editTextModuleCredit1);
+                        String sCreditModule = creditModule.getText().toString();
+                        if (sExamGrade.matches("") || sCwGrade.matches("") || sExamPercentage.matches("") || sCreditModule.matches("")) {
+                            AlertDialog.Builder x = new AlertDialog.Builder(MainActivity.this);
 
-                    double examPercentage = exampercentage;
-                    double courseworkPercentage = 100.0 - examPercentage;
-                    double tempResult = (((grade * examPercentage) / 100.0) + (cwgrade * courseworkPercentage / 100.0)) / 100 * 4 * credit / 10;
-                    totalGPA += tempResult;
-                    totalCredit += credit / 10.0;
-                }
+                            x.setTitle("MISSING VALUE");
+                            x.setMessage("No EMPTY box is allowed\nPlease check the inputs ^-^");
+                            x.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            AlertDialog alert = x.create();
+                            alert.show();
+                        }
+                        else {
+                            double dExamGrade=Double.parseDouble(sExamGrade);
+                            double dCwGrade=Double.parseDouble(sCwGrade);
+                            double dExamPercentage=Double.parseDouble(sExamPercentage);
+                            double dCreditModule = Double.parseDouble(sCreditModule);
+                            double tempMark = ((dExamGrade*dExamPercentage/100.0) + (dCwGrade*(100-dExamPercentage)/100.0))*dCreditModule/10.0;
+                            totalMark+=tempMark;
+                            totalCredit+=(dCreditModule/10.0);
+//                            clear everything
+                            examGrade.getText().clear();
+                            cwGrade.getText().clear();
+                            examPercentage.getText().clear();
+                            creditModule.getText().clear();
+//                            set the result TextView into next one
+                            counter++;
+                            module.setText("Module " + String.valueOf(counter));
+                            result.setText("Saved:" + " Module "+ String.valueOf(counter-1));
+                        }
+                    }
+                });
 
-                double resultFinal = totalGPA/totalCredit;
-                result.setText(String.valueOf(resultFinal));
-                if (resultFinal>=3.5){
-                    result.setText("A : First Class Degree (" + String.valueOf(resultFinal) + ")");
-                }
-                else if (resultFinal<3.5 && resultFinal>=3){
-                    result.setText("B : Second Upper Class Degree (" + String.valueOf(resultFinal)+ ")");
-                }
-                else if (resultFinal<3 && resultFinal>=2.5){
-                    result.setText("B- : Second Lower Class Degree (" + String.valueOf(resultFinal)+ ")");
-                }
-                else if (resultFinal<2.5 && resultFinal>=2.0){
-                    result.setText("General Degree (" + String.valueOf(resultFinal) + ")");
-                }
-                else{
-                    result.setText("Fail : Remodule (" + String.valueOf(resultFinal) + ")");
-                }
-            }
-        });
+                buttonCalc.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        examGrade = (EditText) findViewById(R.id.editTextGrade1);
+                        String sExamGrade = examGrade.getText().toString();
+                        cwGrade = (EditText) findViewById(R.id.editTextCourseWorkGrade1);
+                        String sCwGrade = cwGrade.getText().toString();
+                        examPercentage = (EditText) findViewById(R.id.editTextPercentage1);
+                        String sExamPercentage = examPercentage.getText().toString();
+                        creditModule = (EditText) findViewById(R.id.editTextModuleCredit1);
+                        String sCreditModule = creditModule.getText().toString();
+                        if (sExamGrade.matches("") || sCwGrade.matches("") || sExamPercentage.matches("") || sCreditModule.matches("")) {
+                            AlertDialog.Builder x = new AlertDialog.Builder(MainActivity.this);
+
+                            x.setTitle("MISSING VALUE");
+                            x.setMessage("No EMPTY box is allowed\nPlease check the inputs ^-^");
+                            x.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            AlertDialog alert = x.create();
+                            alert.show();
+                        }
+                        else {
+                            double dExamGrade=Double.parseDouble(sExamGrade);
+                            double dCwGrade=Double.parseDouble(sCwGrade);
+                            double dExamPercentage=Double.parseDouble(sExamPercentage);
+                            double dCreditModule = Double.parseDouble(sCreditModule);
+                            double tempMark = ((dExamGrade*dExamPercentage/100.0) + (dCwGrade*(100-dExamPercentage)/100.0))*dCreditModule/10.0;
+                            totalMark+=tempMark;
+                            totalCredit+=(dCreditModule/10.0);
+//                            clear everything
+                            examGrade.getText().clear();
+                            cwGrade.getText().clear();
+                            examPercentage.getText().clear();
+                            creditModule.getText().clear();
+//                            set module name back
+                            counter =1;
+                            module.setText("Module "+String.valueOf(counter));
+//                            calculate the final result
+                            double finalResult = totalMark/totalCredit;
+                            result.setText(String.valueOf(finalResult));
+                            if(finalResult>=70){
+                                result.setText("GPA - 4.0 (A)\n FIRST-CLASS HONOURS");
+                            }else if(finalResult>=65&&finalResult<=69){
+                                result.setText("GPA - 3.7 (B)\n UPPER SECOND-CLASS HONOURS");
+                            }else if(finalResult>=60&&finalResult<=64){
+                                result.setText("GPA - 3.3 (B)\n UPPER SECOND-CLASS HONOURS");
+                            }else if(finalResult>=55&&finalResult<=59){
+                                result.setText("GPA - 3 (C)\n LOWER SECOND-CLASS HONOURS");
+                            }else if(finalResult>=50&&finalResult<=59){
+                                result.setText("GPA - 2.7 (C)\n LOWER SECOND-CLASS HONOURS");
+                            }else if(finalResult>=45&&finalResult<=49){
+                                result.setText("GPA - 2.3 (D)\n ORDINARY/UNCLASSIFIED");
+                            }else{
+                                result.setText("FAIL\nREMODULE");
+                            }
+                            totalCredit=0;
+                            totalMark=0;
+                        }
+                    }
+                });
             }
 
     }
